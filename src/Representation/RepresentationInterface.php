13ece4ad17d1aa5d6c1a332fb390dcb3aaa64184
fschmtt <frederik.schmitt96@gmail.com>
2021-05-21 23:53:31 +0200

    Add ReadOnlyException

--- src/Representation/RepresentationInterface.php
+++ src/Representation/RepresentationInterface.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Fschmtt\Keycloak\Representation;
 
+use Fschmtt\Keycloak\Exception\ReadOnlyException;
+
 interface RepresentationInterface
 {
     public static function from(array $properties): static;
 
+    /**
+     * @throws ReadOnlyException
+     */
     public function with(string $property, mixed $value): static;
 }
