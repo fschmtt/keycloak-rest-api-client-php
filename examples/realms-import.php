5a4f4501dc4a9263a5d36ef02855e3fcbb60273c
fschmtt <frederik.schmitt96@gmail.com>
2021-05-26 15:50:08 +0200

    Refactor and unify naming of examples

---
+++ examples/realms-import.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Fschmtt\Keycloak\Keycloak;
+use Fschmtt\Keycloak\Representation\Realm;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$keycloak = new Keycloak(
+    baseUrl: 'http://keycloak:8080',
+    username: 'admin',
+    password: 'admin'
+);
+
+$random = bin2hex(random_bytes(length: 8));
+
+$keycloak->realms()->import(
+    new Realm(
+        displayName: 'My Random Realm ' . $random,
+        id: 'my-random-realm-' . $random,
+        realm: 'my-random-realm-' . $random,
+    )
+);
