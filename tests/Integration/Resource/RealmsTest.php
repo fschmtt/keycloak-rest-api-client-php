f22162b296da55da79d510d2e05366e2616049fa
fschmtt <frederik.schmitt96@gmail.com>
2021-11-29 14:03:51 +0100

    Add test cases for Realms::clients() and Realms::users()

--- tests/Integration/Resource/RealmsTest.php
+++ tests/Integration/Resource/RealmsTest.php
@@ -14,7 +14,7 @@ class RealmsTest extends IntegrationTestCase
         $realms = $this->keycloak->realms()->all();
 
         foreach ($realms as $realm) {
-            self::assertInstanceOf(Realm::class, $realm);
+            static::assertInstanceOf(Realm::class, $realm);
         }
     }
 
@@ -22,19 +22,19 @@ class RealmsTest extends IntegrationTestCase
     {
         $realm = $this->keycloak->realms()->get(realm: 'master');
 
-        self::assertEquals('master', $realm->getRealm());
+        static::assertEquals('master', $realm->getRealm());
     }
 
     public function testCanUpdateRealm(): void
     {
         $realm = $this->keycloak->realms()->get(realm: 'master');
 
-        self::assertFalse($realm->getRegistrationAllowed());
+        static::assertFalse($realm->getRegistrationAllowed());
 
         $realm = $realm->withRegistrationAllowed(true);
         $realm = $this->keycloak->realms()->update($realm);
 
-        self::assertTrue($realm->getRegistrationAllowed());
+        static::assertTrue($realm->getRegistrationAllowed());
     }
 
     public function testCanImportRealm(): void
@@ -42,18 +42,38 @@ class RealmsTest extends IntegrationTestCase
         $realm = new Realm(id: 'testing-id', realm: 'testing-realm');
         $realm = $this->keycloak->realms()->import(realm: $realm);
 
-        self::assertEquals('testing-id', $realm->getId());
-        self::assertEquals('testing-realm', $realm->getRealm());
+        static::assertEquals('testing-id', $realm->getId());
+        static::assertEquals('testing-realm', $realm->getRealm());
 
-        self::assertCount(2, $this->keycloak->realms()->all());
+        static::assertCount(2, $this->keycloak->realms()->all());
     }
 
-    public function testCanClearCaches()
+    public function testCanClearCaches(): void
     {
+        static::expectNotToPerformAssertions();
+        
         $realm = new Realm(realm: 'master');
 
         $this->keycloak->realms()->clearKeysCache($realm);
         $this->keycloak->realms()->clearRealmCache($realm);
         $this->keycloak->realms()->clearUserCache($realm);
+    }
+
+    public function testCanGetClients(): void
+    {
+        $realm = new Realm(realm: 'master');
+
+        $clients = $this->keycloak->realms()->clients($realm);
+
+        static::assertCount(7, $clients);
+    }
+
+    public function testCanGetUsers(): void
+    {
+        $realm = new Realm(realm: 'master');
+
+        $users = $this->keycloak->realms()->users($realm);
+
+        static::assertCount(1, $users);
     }
 }
