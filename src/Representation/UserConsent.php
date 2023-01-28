9b53919a4199c51fcdace69b69ec0ecb2353e1b7
Frederik Schmitt <f.schmitt@shopware.com>
2023-01-28 18:15:55 +0100

    Increase phpstan level to 4

--- src/Representation/UserConsent.php
+++ src/Representation/UserConsent.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace Fschmtt\Keycloak\Representation;
 
 /**
+ * @method string|null getClientId()
+ * @method int|null getCreatedDate()
+ * @method array|null getGrantedClientScopes()
+ * @method int|null getLastUpdatedDate()
+ *
  * @codeCoverageIgnore
  */
 class UserConsent extends Representation
 {
-    private ?string $clientId;
-
-    private ?int $createdDate;
-
-    /**
-     * @var string[]|null
-     */
-    private ?array $grantedClientSCopes;
-
-    private ?int $lastUpdatedDate;
+    public function __construct(
+        protected ?string $clientId,
+        protected ?int $createdDate,
+        protected ?array $grantedClientScopes,
+        protected ?int $lastUpdatedDate,
+    ) {
+    }
 }
