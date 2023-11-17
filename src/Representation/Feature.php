5ea0c58d1698ecdfcba43c416946c666aa5e716a
Frederik Schmitt <f.schmitt@shopware.com>
2023-11-17 15:57:54 +0100

    Add Feature and FeatureCollection

---
+++ src/Representation/Feature.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fschmtt\Keycloak\Representation;
+
+/**
+ * @method string|null getName()
+ * @method string|null getLabel()
+ * @method string|null getType()
+ * @method string[]|null getDependencies()
+ * @method bool|null getEnabled()
+ *
+ * @codeCoverageIgnore
+ */
+class Feature extends Representation
+{
+    public function __construct(
+        protected ?string $name = null,
+        protected ?string $label = null,
+        protected ?string $type = null,
+        protected ?array $dependencies = null,
+        protected ?bool $enabled = null,
+    ) {
+    }
+}
